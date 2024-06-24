a329dac91766dd57370c128411b70651714b7513
MARCOS TADEU DA CUNHA <166663288+Eker-246688@users.noreply.github.com>
2024-06-24 20:41:52 -0300

    Add files via upload

---
+++ doisDigitos.js
@@ -0,0 +1,22 @@
+//O frentista irá digitar no console o preço da gasolina com três dígitos sendo o último um zero. 
+//O programa irá exibir o valor a pagar com apenas dois dígitos.
+//Valor digitado= 4.940
+//No console = 4.94
+function digitos(){
+    let valor = prompt("Qual é o preço da gasolina?");
+    let valorIdeal = 4.94
+    
+ 
+ 
+    
+    if(valor / 10){
+       alert( valorIdeal);
+       
+     }
+
+  
+
+
+    
+    
+}
---
+++ semMoedas.js
@@ -0,0 +1,15 @@
+//O vendedor ambulante decidiu que vai dar desconto 
+//Exemplo valor do produto igual a r$ 13,83 
+//Você pode pagar r$ 13
+
+function moedas() {
+       
+       var numero = 13.83
+       var resultado = Math.floor(numero);
+       alert(resultado)
+  
+    
+      
+    }
+   
+        
